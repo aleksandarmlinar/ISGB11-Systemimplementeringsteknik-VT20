24566dd6d6282118d757b8d0efc0d19c1bf26b57
Peter Bellström <peter.bellstrom@kau.se>
2020-03-17 17:24:27 +0100

    Uppdaterat ex4_slutkod

--- Forelasning3/ex4_slutkod.php
+++ Forelasning3/ex4_slutkod.php
@@ -1,5 +1,70 @@
 <?php
 	//Här kommer koden...
+
+	function deleteSession() {
+
+		session_unset();
+
+		if( ini_get("session.use_cookies") ) {
+
+			$sessionCookieData = session_get_cookie_params();
+
+			$path = $sessionCookieData["path"];
+			$domain = $sessionCookieData["domain"];
+			$secure = $sessionCookieData["secure"];
+			$httponly = $sessionCookieData["httponly"];
+
+			$name = session_name();
+
+			setcookie($name, "", time() - 3600, $path, $domain, $secure, $httponly);
+
+		}
+
+		session_destroy();
+
+	}
+
+	session_start();
+	session_regenerate_id( true );
+
+	$css = "body { color: rgb(0, 0, 0); background-color: rgb(255, 255, 255); }";
+	$disabled = true;
+
+	if( isset( $_POST["btnSend"])) {
+
+		$fgColor = $_POST["foregroundcolor"];
+		$bgColor = $_POST["backgroundcolor"];
+
+		$_SESSION["fgColor"] = $fgColor;
+		$_SESSION["bgColor"] = $bgColor;
+
+		$css = "body{ color: $fgColor; background-color: $bgColor }";
+
+		$disabled = false;
+
+	}
+
+	if( isset($_POST["btnReset"])){
+		deleteSession();
+	}
+
+	if(!isset($_POST["btnSend"]) && !isset($_POST["btnReset"]) && isset($_SESSION["bgColor"]) && isset($_SESSION["fgColor"])) {
+		
+		$fgColor = $_SESSION["fgColor"];
+		$bgColor = $_SESSION["bgColor"];
+
+		$css = "body{ color: $fgColor; background-color: $bgColor }";
+
+		$disabled = false;
+	}
+
+	if(!isset($_POST["btnSend"]) && 
+		!isset($_POST["btnReset"]) && 
+		!isset($_SESSION["bgColor"]) && 
+		!isset($_SESSION["fgColor"])) {
+			deleteSession();
+	}
+
 ?>
 <!doctype html>
 <html lang="en" >
@@ -9,6 +74,7 @@
 		<style>
 			<?php
 				//Här en utskrift av CSS-text!
+				echo( $css );
 			?>
 		</style>
 	</head>
@@ -30,7 +96,7 @@
 				</select>
 			
 				<input type="submit" name="btnSend" value="Send" />
-				<input type="submit" name="btnReset" value="Reset" />
+				<input type="submit" name="btnReset" value="Reset" <?php if( $disabled ) { echo("disabled"); } ?> />
 			
 			</form>
 		
